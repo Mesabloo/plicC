9e542dd807f0bc2c3386f8ad98f381a51cc56f64
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-03-19 12:18:12 +0100

    Plic-1: remove typechecker and code generator

--- src/plic/generator/mips/MIPSGenerator.java
+++
@@ -1,8 +0,0 @@
-package plic.generator.mips;
-
-import plic.generator.Generator;
-import plic.typechecker.core.SymbolTable;
-
-public abstract class MIPSGenerator implements Generator {
-    public static SymbolTable symbols;
-}
